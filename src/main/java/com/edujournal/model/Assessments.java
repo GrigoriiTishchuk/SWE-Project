875edc100136820d479b97d203e686d3c1325e7a
petrova-olena <petrova.olena.anatoliivna@gmail.com>
2026-09-06 20:29:18 +0300

    Add Assessments and Grades entities to the package "model"

---
+++ src/main/java/com/edujournal/model/Assessments.java
@@ -0,0 +1,50 @@
+package com.edujournal.model;
+
+import java.time.LocalDate;
+
+public class Assessments {
+    private int id;
+    private int courseId;
+    private String title;
+    private String type;
+    private double maxScore;
+    private double weight;
+    private LocalDate dueDate;
+
+    public Assessments(int id, int courseId, String title, String type, double maxScore, double weight, LocalDate dueDate) {
+        this.id = id;
+        this.courseId = courseId;
+        this.title = title;
+        this.type = type;
+        this.maxScore = maxScore;
+        this.weight = weight;
+        this.dueDate = dueDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getType() {
+        return type;
+    }
+    public double getMaxScore() {
+        return maxScore;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+}
